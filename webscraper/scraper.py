42b8c3b282379f659ce63132a4cebb98f2180a6a
Ashwin Dharne <ashwindharne@dm.ai>
2018-06-02 16:51:59 -0700

    scraping updated

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -8,9 +8,15 @@ import numpy as np
 import requests
 import shutil
 from bs4 import BeautifulSoup
-from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
+cred = credentials.Certificate("credentials/credentials.json")
 
+firebase_admin.initialize_app(cred,options={
+    'databaseURL': 'https://smartfit-3ad0b.firebaseio.com/'
+})
 
 def main():
     scrape('https://www.farfetch.com/shopping/women/alexander-wang-cult-straight-leg-jeans-item-12810274.aspx?storeid=10168')
@@ -18,16 +24,20 @@ def main():
 def scrape(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content.decode('utf-8'), 'html.parser')
-    for item in soup.find_all(itemprop='brand'):
-        print(item.text)
-
-def avgColor(url):
-    print('avg')
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        with open('temp/img.jpeg', 'wb') as image:
-            image.write(resp.content)
-
+    brand = soup.find(itemprop='brand').text
+    print(brand)
+    name = soup.find(itemprop='name').text.lstrip()
+    print(name)
+    price = soup.find(itemprop='price')['content']
+    print(price)
+    images = soup.find_all(itemprop='image', alt=True)
+    for img in images:
+        if 'cdn-images' in img.prettify() and 'data-large' in img.prettify():
+            image=img['data-large']
+            break
+    print(image)
+    composition = soup.find_all('dt')
+    print(composition)
 
 if __name__ == '__main__':
     main()
