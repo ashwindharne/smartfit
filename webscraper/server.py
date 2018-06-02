b35326b8c61d54f06da42dc94eafdfe91a7e0501
Jennie Zheng <jenniezheng321@gmail.com>
2018-06-02 14:39:38 -0700

    firebase usage

--- webscraper/server.py
+++ webscraper/server.py
@@ -1,7 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import flask
+from flask import jsonify
 
-@app.route('/item/<string:id>')
+app = flask.Flask(__name__)
+cred = credentials.Certificate("credentials/credentials.json")
+
+firebase_admin.initialize_app(cred,options={
+    'databaseURL': 'https://smartfit-3ad0b.firebaseio.com/'
+})
+
+root = db.reference('items')
+
+@app.route('/item/<int:id>')
 def show_subpath(id):
-    # show the subpath after /path/
-    return 'id %s' % id
+    item = root.child('denim').child(str(id)).get()
+    if not item:
+        print("Invalid Item!")
+        return '{}'
+    return jsonify(item)
+
+@app.route('/recommend')
+def show_subpath(id):
+    currentID = request.args.get('currentID')
+    if not item:
+        print("Invalid Item!")
+        return '{}'
+    return jsonify(item)
