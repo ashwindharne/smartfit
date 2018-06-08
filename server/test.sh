a66d8d5cada6df019f06a38d5ae0363819dd4087
ashwindharne <ashwindharne@dm.ai>
2018-06-07 18:48:12 -0700

    added instructions to test.sh

--- server/test.sh
+++ server/test.sh
@@ -1,3 +1,4 @@
 #make sure all dependencies are installed, and nose is also installed.
 # ./test.sh will run all 7 tests for the request API
+# Run server in background, localhost and port 5000
 nosetests -v tests.py
